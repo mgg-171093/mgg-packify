4afb0c853a404299ccb67e9fdcc48a9a0e099edb
Manuel <manuel_171093@hotmail.com>
2026-04-12 15:34:33 -0500

    chore(release): bump version to 3.9.0

--- app/lib/core/constants.dart
+++ app/lib/core/constants.dart
@@ -1,5 +1,5 @@
 const int kApiPort = 8787;
 const String kBaseUrl = 'http://127.0.0.1:8787';
-const String kAppVersion = '3.8.0';
+const String kAppVersion = '3.9.0';
 const String kUpdateCheckUrl =
     'https://raw.githubusercontent.com/mgg-171093/mgg-packify/main/latest.json';
